d1276d4deb6174933331b89cc1ddd95b0c25c655
Ian Malerich <ian.malerich@gmail.com>
2017-01-28 21:13:05 +0000

    problem 1 should be done

--- hw1/p1.m
+++ hw1/p1.m
@@ -1,3 +1,22 @@
 function p1
 
+clear all;
+close all;
+
+% Actual curve.
+_x = linspace(-1, 1, 200);
+_y = 1 ./ (1 + 25 * _x .^ 2);
+
+% Interpolating points.
+x = -1:0.25:1;
+y = 1 ./ (1 + 25 * x .^ 2);
+
+% Fit a polynomial to the interpolating points.
+p = polyval(polyfit(x, y, 5), _x);
+
+hold on;
+plot(_x, _y, 'b', 'LineWidth', 3);
+plot(_x,  p, 'r', 'LineWidth', 3);
+scatter(x, y, 50, 'r', 'filled');
+
 end
