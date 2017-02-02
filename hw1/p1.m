49e7b868c11cde57bc10bdee3e413e88943f4cef
Ian Malerich <ian.malerich@gmail.com>
2017-02-02 05:46:40 +0000

    integral approximation should be working now

--- hw1/p1.m
+++ hw1/p1.m
@@ -2,6 +2,7 @@ function p1
 
 clear all;
 close all;
+hold on;
 
 f = @(x) 1 ./ (1 + 25 * x .^ 2);
 
@@ -16,7 +17,6 @@ y = f(x);
 % Fit a polynomial to the interpolating points.
 p = polyval(polyfit(x, y, 5), _x);
 
-hold on;
 plot(_x, _y, 'b', 'DisplayName', 'Actual', 'LineWidth', 3);
 plot(_x,  p, 'r', 'DisplayName', 'Interpolating', 'LineWidth', 3);
 scatter(x, y, 50, 'r', 'filled');
--- hw1/p2.m
+++ hw1/p2.m
@@ -5,18 +5,17 @@ close all;
 hold on;
 
 % Constants used for testing.
-h = 0.5;
-x0 = 1.0;
+h = 1.0;
+x0 = 2.0;
 
 % Example function to test our interpolation method.
 f = @(x) e .^ (-x);
-_x = linspace(0, 2, 200);
+_x = linspace(0, 3, 200);
 
 % List of our interpolation points.
 x = [x0 - 2*h, x0, x0 + h];
 
 % Plot our actual curve, and the points we are interpolating around.
-hold on;
 plot(_x, f(_x), 'b', 'DisplayName', 'e^{-x}', 'LineWidth', 3);
 
 % Next define the components of our interpolating function.
@@ -32,6 +31,28 @@ L2 = @(x) (x - x0 + 2 * h) .* (x - x0) .* (1 / (3 * (h .^ 2)));
 P = @(x) F0 * L0(x) + F1 * L1(x) + F2 * L2(x);
 plot(_x, P(_x), 'r', 'DisplayName', 'Lagrange', 'LineWidth', 3);
 scatter(x, f(x), 75, 'k', 'filled', 'DisplayName', 'Interpolating Points');
+
+% Compute the integral for P(x).
+intL0 = @(t) (-h/2.0) .* (t .^ 2) + (h .* x0 .* t) + (1/3) .* (t .^ 3)  - (x0 .* (t .^ 2)) + ((x0 .^ 2) .* t);
+intL1 = @(t) (-2 * (h .^ 2) .* t) + (h/2) .* (t .^ 2) - (h * x0) .* t + (1/3) .* (t .^ 3) - (x0 .* (t .^ 2)) + (x0 .^ 2) .* t;
+intL2 = @(t) h .* (t .^ 2) - (2 * h * x0) .* t + (1/3) .* (t .^ 3) - x0 .* (t .^ 2) + (x0 .^ 2) .* t;
+
+% Evaluate the above integrals over the bound (a, b).
+a = x0 - 2 * h;
+b = x0 + h;
+
+evalL0 = intL0(b) - intL0(a);
+evalL1 = intL1(b) - intL1(a);
+evalL2 = intL2(b) - intL2(a);
+
+% Calculate the partial sums that can be summed to estimate our integral.
+k0 = (1 / ( 6 * (h ^ 2))) * f(x0 - 2 * h) * evalL0;
+k1 = (1 / (-2 * (h ^ 2))) * f(x0) * evalL1;
+k2 = (1 / ( 3 * (h ^ 2))) * f(x0 + h) * evalL2;
+
+quad(f, 0, 3) % Actual
+int = k0 + k1 + k2 % Our int(P(x))
+quad(P, 0, 3) % Matlab int(P(x))
 
 legend('show');
 xlabel('x');
